915dad9ecf70866b936ac7ff1b5a3ae9f200f1e2
Ing-Abrahan-Jimenez <169616802+Ing-Abrahan-Jimenez@users.noreply.github.com>
2024-05-19 13:16:14 -0600

    initial commit

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 st.header('Lanzar una moneda')
+num_of_trails = st.slider('¿Número de intentos',1,1000,10)
+start_button = st.button('Ejecutar')
+if start_button:
+    st.write(f'experimento con {num_of_trails} intentos en curso.')
+
 st.write('Esta app aún se encuentra en desarrollo')
