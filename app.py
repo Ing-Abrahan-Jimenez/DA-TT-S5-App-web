facdd6fb9370256556ea925cf8ca31ecd0f22d7b
Ing-Abrahan-Jimenez <169616802+Ing-Abrahan-Jimenez@users.noreply.github.com>
2024-05-19 13:41:24 -0600

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import scipy.stats
 import time
+import pandas as pd
+
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'interactions', 'media'])
+
+
 st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
@@ -25,4 +33,10 @@ start_button = st.button('Ejecutar')
 if start_button:
     st.write(f'Experimento con {num_of_trails} intentos en curso.')
     mean = toss_coin(num_of_trails)
-st.write('Esta app aún se encuentra en desarrollo')
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],num_of_trails, mean]], columns=['no', 'iterarions', 'mean'])     
+    ], axis= 0)
+    st.session_state['df_experiment_results']= \\
+    st.session_state['df_experiment_results'].reset_index(drop=True)
+st.write(st.session_state['df_experiment_results'])
